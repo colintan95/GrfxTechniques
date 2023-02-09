a6784b49f1148402044d8686b3dc22c2f95e4857
Colin Tan <colintan95@gmail.com>
2023-02-08 17:46:55 -0800

    Added WinMain

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <windows.h>
+
+static LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
+{
+    switch (message)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+    }
+
+    return DefWindowProc(hwnd, message, wparam, lparam);
+}
+
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE, LPSTR, int cmdShow)
+{
+    WNDCLASSEX windowClass{};
+    windowClass.cbSize = sizeof(WNDCLASSEX);
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    windowClass.lpfnWndProc = WindowProc;
+    windowClass.hInstance = hinstance;
+    windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    windowClass.lpszClassName = L"GrfxTechniques";
+    RegisterClassEx(&windowClass);
+
+    HWND hwnd = CreateWindow(windowClass.lpszClassName, L"Grfx Techniques", WS_OVERLAPPEDWINDOW,
+                             CW_USEDEFAULT, CW_USEDEFAULT, 1024, 768, nullptr, nullptr, hinstance,
+                             nullptr);
+    ShowWindow(hwnd, cmdShow);
+
+    CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+
+    MSG msg{};
+
+    while (msg.message != WM_QUIT)
+    {
+        if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return 0;
+}
+
+
+
